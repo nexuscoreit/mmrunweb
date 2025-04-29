aab5349f87736f50ac66335e90fc22f49adecc5d
JPiccat <johanapiccat00@gmail.com>
2025-04-29 09:34:38 -0300

    Elimino create db.run

--- backend/models/inscripcion.js
+++ backend/models/inscripcion.js
@@ -1,24 +1,4 @@
-// backend/models/inscripcion.js
 const db = require('./db');
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS inscripciones (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT,
-    apellido TEXT,
-    dni TEXT UNIQUE,
-    genero TEXT,
-    fechaNacimiento TEXT,
-    email TEXT,
-    telefono TEXT,
-    ciudad TEXT,
-    categoria TEXT,
-    talle TEXT,
-    descuento TEXT,
-    pagado INTEGER DEFAULT 0,
-    fechaRegistro TEXT DEFAULT CURRENT_TIMESTAMP
-  )
-`);
 
 const crearInscripcion = (data, callback) => {
   const {
